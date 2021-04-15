fb26e3378f1e75e1f8352cf01fb746c42f40fadf
bessah <36328573+bessah@users.noreply.github.com>
2021-04-15 10:45:21 +0200

    US-1 : Add Money class

---
+++ src/main/java/com/bs/bank/account/kata/Money.java
@@ -0,0 +1,34 @@
+package com.bs.bank.account.kata;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Money add(Money other) {
+        return new Money(this.amount.add(other.amount));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount.equals(money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return amount.toString();
+    }
+}
---
+++ src/test/java/com/bs/bank/account/kata/MoneyShould.java
@@ -0,0 +1,24 @@
+package com.bs.bank.account.kata;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MoneyShould {
+
+    @Test
+    void add_money() {
+        // Given
+        Money money1 = new Money(BigDecimal.valueOf(1000));
+        Money money2 = new Money(BigDecimal.valueOf(150));
+
+        // When
+        Money actual = money1.add(money2);
+
+        // Then
+        Money expected = new Money(BigDecimal.valueOf(1150));
+        assertEquals(expected, actual);
+    }
+}
